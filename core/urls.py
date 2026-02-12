2e49796e7f54d5d335a78b17d02e550d7c6b6ba8
albertomancera <152221655+albertomancera@users.noreply.github.com>
2026-02-12 13:59:42 +0100

    Hasta el LOGIN y REGISTRO

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from viajes.views import inicio, inscribirse
-from cuentas.views import registro 
+from viajes import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio, name='inicio'),
-    path('inscribirse/<int:viaje_id>/', inscribirse, name='inscribirse'),
+    path('', views.inicio, name='inicio'),
+    path('registro/', views.registro, name='registro'),
+    path('inscribirse/<int:viaje_id>/', views.inscribirse, name='inscribirse'),
     
-    # Rutas de Cuentas
-    path('cuentas/', include('django.contrib.auth.urls')), 
-    path('registro/', registro, name='registro'),
+    # Sistema de login/logout
+    path('cuentas/', include('django.contrib.auth.urls')),
 ]
